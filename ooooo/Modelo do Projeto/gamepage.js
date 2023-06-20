dcabb43844164c536a7d4a14fe712e607375c897
foxy0980 <133064291+foxy0980@users.noreply.github.com>
2023-06-20 19:15:26 -0300

    Add files via upload

---
+++ ooooo/Modelo do Projeto/gamepage.js
@@ -0,0 +1,12 @@
+// geração do valor aleatório
+var y = math.floor(math.random()  *  10 + 1  );
+// contagem de tentativas
+// até acerto    
+  
+// função para o número enviado pelo usuário
+var x = document.getElementById("guessField").value;
+   
+
+
+
+
